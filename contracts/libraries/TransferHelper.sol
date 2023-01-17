95bd9efe49855f5e5bbbff4ddb9dfb63ece0ad77
Alex Michelsen <57596674+icemanparachute@users.noreply.github.com>
2023-01-17 11:19:56 -0500

    update to just sending Eth in transferPayment

--- contracts/libraries/TransferHelper.sol
+++ contracts/libraries/TransferHelper.sol
@@ -56,20 +56,14 @@ library TransferHelper {
   ) internal {
     if (token == weth) {
       require(msg.value == amount, 'THL03');
-      if (!Address.isContract(to)) {
-        (bool success, ) = to.call{value: amount}('');
-        require(success, 'THL04');
-      } else {
-        /// @dev we want to deliver WETH from ETH here for better handling at contract
-        IWETH(weth).deposit{value: amount}();
-        assert(IWETH(weth).transfer(to, amount));
-      }
+      (bool success, ) = to.call{value: amount}('');
+      require(success, 'THL04');
     } else {
       transferTokens(token, from, to, amount);
     }
   }
 
-  /// @dev Internal funciton that handles withdrawing tokens and WETH that are up for sale to buyers
+  /// @dev Internal function that handles withdrawing tokens and WETH that are up for sale to buyers
   /// @dev this function is only called if the tokens are not timelocked
   /// @dev this function handles weth specially and delivers ETH to the recipient
   function withdrawPayment(
